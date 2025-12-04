4757e446178fc4b0ba43c13288714e695a6bc51e
Madhubabu329 <madhubabuk21@gmail.com>
2025-12-04 11:13:13 +0530

    delpoyee same

--- utils/generateOtp.js
+++ utils/generateOtp.js
@@ -18,15 +18,15 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // App Password recommended
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD, // App Password recommended
   },
 });
 
 const sendEmail = async (to, subject, text) => {
   try {
     const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: process.env.EMAIL,
       to,
       subject,
       text,
@@ -38,3 +38,17 @@ const sendEmail = async (to, subject, text) => {
 };
 
 module.exports = sendEmail;
+
+
+
+// const bcrypt = require("bcryptjs");
+
+// // Only generates OTP and hashed code
+// const generateOtp = async () => {
+//   const code = Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit OTP
+//   const salt = await bcrypt.genSalt(10);
+//   const hashed = await bcrypt.hash(code, salt);
+//   return { code, hashed };
+// };
+
+// module.exports = generateOtp;
